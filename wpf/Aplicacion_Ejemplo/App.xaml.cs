b8e0d479ba8d42c0a7fb7de1022625064e3dc3a7
VRamirez <vicenteoscarramirezgarrido@gmail.com>
2025-11-20 14:56:20 -0300

    Add files via upload

---
+++ wpf/Aplicacion_Ejemplo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aplicacion_Ejemplo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
